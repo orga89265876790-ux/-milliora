fda154ec9841d922500552034d744cdfa9526e0b
orga89265876790-ux <orga89265876790@gmail.com>
2026-08-22 03:01:31 +0300

    Temporarily block MilliOra from search indexing

--- app/robots.ts
+++ app/robots.ts
@@ -1,20 +1,12 @@
 import type { MetadataRoute } from "next";
-import { siteUrl } from "@/lib/site";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
         userAgent: "*",
-        allow: "/",
-        disallow: ["/api/"],
+        disallow: "/",
       },
     ],
-    ...(siteUrl
-      ? {
-          sitemap: `${siteUrl}/sitemap.xml`,
-          host: siteUrl,
-        }
-      : {}),
   };
 }
